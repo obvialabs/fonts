ec0c48827d16357c6037a82cb2edf6624452ef20
selcukcukur <selcukcukur@outlook.com.tr>
2026-03-24 16:16:50 +0300

    fix(fonts): rebuild react implementation

---
+++ packages/fonts/dist/react.js
@@ -0,0 +1,129 @@
+import "./fonts/fonts.css";
+/**
+ * **Obvia Sans** font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `100` - Thin
+ * - `200` - Ultra Light
+ * - `300` - Light
+ * - `400` - Regular
+ * - `500` - Medium
+ * - `600` - Semibold
+ * - `700` - Bold
+ * - `800` - Black
+ * - `900` - Ultra Black
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaSans } from "@obvia/fonts/react"
+ *
+ * export const ObviaSans ({ children, className }) => (
+ *   <span className={`${obviaSans}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaSans = "font-obvia-sans";
+/**
+ * **Obvia Mono** variable font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `100` - Thin
+ * - `200` - Ultra Light
+ * - `300` - Light
+ * - `400` - Regular
+ * - `500` - Medium
+ * - `600` - Semibold
+ * - `700` - Bold
+ * - `800` - Black
+ * - `900` - Ultra Black
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaMono } from "@obvia/fonts/react"
+ *
+ * export const ObviaMono ({ children, className }) => (
+ *   <span className={`${obviaMono}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaMono = "font-obvia-mono";
+/**
+ * **Obvia Pixel Square** variable font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `500` - Medium
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaPixelSquare } from "@obvia/fonts/react"
+ *
+ * export const ObviaPixelSquare = ({ children, className }) => (
+ *   <span className={`${obviaPixelSquare}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaPixelSquare = "font-obvia-pixel-square";
+/**
+ * **Obvia Pixel Grid** variable font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `500` - Medium
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaPixelGrid } from "@obvia/fonts/react"
+ *
+ * export const ObviaPixelGrid = ({ children, className }) => (
+ *   <span className={`${obviaPixelGrid}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaPixelGrid = "font-obvia-pixel-grid";
+/**
+ * **Obvia Pixel Circle** variable font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `500` - Medium
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaPixelCircle } from "@obvia/fonts/react"
+ *
+ * export const ObviaPixelCircle = ({ children, className }) => (
+ *   <span className={`${obviaPixelCircle}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaPixelCircle = "font-obvia-pixel-circle";
+/**
+ * **Obvia Pixel Triangle** variable font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `500` - Medium
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaPixelTriangle } from "@obvia/fonts/react"
+ *
+ * export const ObviaPixelTriangle = ({ children, className }) => (
+ *   <span className={`${obviaPixelTriangle}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaPixelTriangle = "font-obvia-pixel-triangle";
+/**
+ * **Obvia Pixel Line** variable font, with `className` and `variable` properties, meant to be attached to DOM elements via `className`
+ *
+ * **Weights**
+ * - `500` - Medium
+ *
+ * **Usage**
+ * ```tsx
+ * import { obviaPixelLine } from "@obvia/fonts/react"
+ *
+ * export const ObviaPixelLine = ({ children, className }) => (
+ *   <span className={`${obviaPixelLine}`}>{children}</span>
+ * )
+ * ```
+ */
+export const obviaPixelLine = "font-obvia-pixel-line";
